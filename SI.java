f8a70d2dde0edeb83d74310c34b2e8b270aa361a
JanJosh <108718031+JanJosh@users.noreply.github.com>
2022-07-16 15:53:25 +0530

    Source Code

--- SI.java
+++ SI.java
@@ -1,16 +1,16 @@
 package com.prodapt.OOPbasics;
 
 public class SI {
-	static int SI;
-	static int principal;
-	static int rate;
-	static int time;
-	static int total_Amount;
+	 static int simple_Interest;
+	 static int principle;
+	 static int rate;
+     static int time;
+	 static int total_Amount;
 	
-	void simpleInterest(int principal,int rate,int time) {
-		SI = ((principal*rate*time)/100);
-		System.out.println("SimpleInterest = " + SI);
-		total_Amount = principal + SI;
+	static void simpleInterest() {
+		simple_Interest = ((principle*rate*time)/100);
+		System.out.println("SimpleInterest = " + simple_Interest);
+		total_Amount = principle + simple_Interest;
 		System.out.println("Amount to be paid after Loan = " + total_Amount);
 	}
 	
