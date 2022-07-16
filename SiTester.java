28bf5a981fd0062af2d619ba4666ea815d715b5a
Harris Joshua S <harrisjoshua.s@prodapt.com>
2022-07-16 12:01:53 +0530

    Tester of SI Code

---
+++ SiTester.java
@@ -0,0 +1,14 @@
+package com.prodapt.OOPbasics;
+
+public class SiTester {
+	public static void main(String[] args) {
+		
+		SI si = new SI();
+		
+		si.simpleInterest(2000, 10, 1);
+		
+	}
+	
+	
+
+}
