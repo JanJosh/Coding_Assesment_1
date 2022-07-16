e003d64b38b2e6b0f235eeef50a5f3c335001436
Harris Joshua S <harrisjoshua.s@prodapt.com>
2022-07-16 12:00:42 +0530

    SimpleInterest source code

---
+++ SI.java
@@ -0,0 +1,18 @@
+package com.prodapt.OOPbasics;
+
+public class SI {
+	static int SI;
+	static int principal;
+	static int rate;
+	static int time;
+	static int total_Amount;
+	
+	void simpleInterest(int principal,int rate,int time) {
+		SI = ((principal*rate*time)/100);
+		System.out.println("SimpleInterest = " + SI);
+		total_Amount = principal + SI;
+		System.out.println("Amount to be paid after Loan = " + total_Amount);
+	}
+	
+
+}
